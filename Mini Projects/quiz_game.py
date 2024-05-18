e31ccfa4b9fb8a64343524703370690016247e1a
Ahmed <ahmed.mca18@gmail.com>
2024-05-19 04:29:39 +0530

    finished quiz_game code

--- Mini Projects/quiz_game.py
+++ Mini Projects/quiz_game.py
@@ -8,28 +8,39 @@ if playing.lower() != "y":
 
 print("Okay, let's play!")
 
+score = 0  # doing this to provide score how many correct answer
+
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1 
 else:
     print("Incorrect!")
         
 
-answer = input("What does GPU stand for? ")
-if answer.lower() == "Graphics processing unit":
+answer = input("What ML stand for? ")
+if answer.lower() == "machine learning":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")        
 
 
-answer = input("What does RAM stand for? ")
-if answer.lower() == "Random Access Memory":
+answer = input("What does DL stand for? ")
+if answer.lower() == "deep learning":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")    
     
-answer = input("What does PSU stand for? ")
-if answer.lower() == "Power Supply":
+answer = input("What does AI stand for? ")
+if answer.lower() == "artificial intelligence":
     print("Correct!")
+    score += 1
 else:
-    print("Incorrect!")        
+    print("Incorrect!")    
+    
+    
+print("You got " + str(score) + " questions corrects!")    
+
+print("you got " + str((score/4) * 100) + " %.")    
