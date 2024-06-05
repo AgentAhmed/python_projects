1d8d5b34a06782dcae4305986257d03ab9e93e1d
Ahmed <ahmed.mca18@gmail.com>
2024-06-05 10:02:58 +0530

    py_to_exe file

---
+++ Multiple projects python/python_to_exe.py
@@ -0,0 +1,6 @@
+# use command - >
+# pyinstaller --onefile filename.pyInstaller
+
+# advance way when e.g game icon file etc required the
+# can add modify advance feature
+# auto-py-to-exe using, check wscubetech video
